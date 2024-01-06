572a455f82902e5bf738de2623e5d535068a3cf4
Karolina Bogacka <bogacka@ibspan.waw.pl>
2024-01-06 23:03:25 +0100

    Correct a small typo

--- inference_application/code/main.py
+++ inference_application/code/main.py
@@ -12,7 +12,7 @@ def serve():
     prometheus_client.start_http_server(9000)
     options = ('grpc.max_send_message_length', 512 * 1024 * 1024), ('grpc.max_receive_message_length', 512 * 1024 * 1024)
     # initialize server with 4 workers
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=200, options=options))
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=200), options=options)
 
     # The setup of the service is flexible and can be seen more thoroughly in classes ServiceManager and InferenceSetupLoader
     ServiceManager().setup_service(server)
